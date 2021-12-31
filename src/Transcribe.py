989141cf6a63c716c17085f03ad9477e05eada91
Kaleb Dutson <kalebdutson11@gmail.com>
2021-12-30 17:51:25 -0700

    Started work on sendind data to DeepSpeech model

--- src/Transcribe.py
+++ src/Transcribe.py
@@ -1,12 +1,31 @@
-import DeepSpeech.transcribe as dpt
+from deepspeech import Model, version
+import os
 
 class Transcribe:
     def __init__(self, file_paths):
         self.file_paths = file_paths
         for file in file_paths:
-            print(file)
-            # self.transcribe_file(file)
+            # print(file)
+            # print("OS:", os.getcwd())
+            # print(file.split(os.getcwd()))
+            self.transcribe_file(file)
 
     def transcribe_file(self, fpath):
-        print("Transcribing file:\n%s" % fpath)
-        dpt.transcribe_file(fpath, None)
+        print(os.getcwd())
+
+        model_path = os.path.join(os.getcwd(), "model/deepspeech-0.9.3-models.pbmm")
+        print("\n\n" + model_path, end="\n\n")
+
+        # TODO: Convert non-WAV files into WAV format
+        # dir_path = os.path.join(os.getcwd(), "tmp")
+        # if not os.path.isdir(dir_path):
+        #     os.makedirs(dir_path)
+
+        ds = Model(model_path)
+
+        # if args.extended:
+        #     print(metadata_to_string(ds.sttWithMetadata(audio, 1).transcripts[0]))
+        # elif args.json:
+        #     print(metadata_json_output(ds.sttWithMetadata(audio, args.candidate_transcripts)))
+        # else:
+        #     print(ds.stt(audio))
